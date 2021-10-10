78ef4a9587da922a83e041eef9954eef4323fbd6
Josh <joshua.andres.blanco@gmail.com>
2021-10-10 08:14:48 -0500

    fix example

--- examples/MCP33121/MCP33121.ino
+++ examples/MCP33121/MCP33121.ino
@@ -1,5 +1,3 @@
-#include <MCP331.h>
-
 /*
 This library can be used for MCP33111, MCP33121, MCP33131 single-ended 
 for ATMEGA328P
@@ -8,9 +6,12 @@ Define first or declare as a variable
 - Number of Bits.
 - Voltage Reference.
 */
+
+#include <MCP331.h>
+
 #define Clock_Division 2
 #define Num_Bits 14
-#define V_Ref 5
+#define V_Ref 5.0
 
 MCP331 mcp331(10, Clock_Division, Num_Bits);
 
@@ -20,9 +21,8 @@ void setup(){
 }
 
 void loop(){
-    uint16_t value = mcp331.readMCP331();
-    double value_temp = (double) value;
-    double voltage = value_temp*(V_Ref/pow(value,2)-1);
+    uint16_t values = mcp331.readMCP331();
+    double voltage = values*(V_Ref/pow(2,Num_Bits)-1);
     Serial.print("Voltage: ");
-    Serial.println(value,4);    
+    Serial.println(voltage,4);    
 }
